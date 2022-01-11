99f2a87595c78406e630e8115ddd0179e71f08a9
pierre <pierre.teodoresco@etu.univ-amu.fr>
2022-01-11 12:15:04 +0100

    css index inspired by twitter

--- index.php
+++ index.php
@@ -11,60 +11,62 @@
     <title>E-Event.io!</title>
 </head>
 
-<body>
-<header>
-    <h1>E-Event.io!</h1>
-</header>
-<div class="wrapper">
-    <div class="sidebar-container">
-        <ul class="sidebar-navigation">
-            <li id="premier">
-                <a href="#">
-                    <i class="fa fa-home" aria-hidden="true"></i> Statut
-                </a>
-            </li>
-            <li id="deuxieme">
-                <a href="#">
-                    <i class="fa fa-tachometer" aria-hidden="true"></i> Dernier donateur :
-                </a>
-            </li>
-            <li id="troisieme">
-                <a href="#">
-                    <i class="fa fa-users" aria-hidden="true"></i>
-                </a>
-            </li>
-            <li id="quatrieme">
-                <a href="#">
-                    <i class="fa fa-cog" aria-hidden="true"></i> Dernier organisateur :
-                </a>
-            </li>
-            <li id="cinquieme">
-                <a href="#">
-                    <i class="fa fa-info-circle" aria-hidden="true"></i>
-                </a>
-            </li>
-            <li id="sixième">
-                <a href="#">
-                    <i class="fa fa-info-circle" aria-hidden="true"></i> Top 5
-                </a>
-            </li>
-            <li id="septième">
-                <a href="#">
-                    <i class="fa fa-info-circle" aria-hidden="true"></i>
-                </a>
-            </li>
-            <li id="LoginPage">
-                <a href="#">
-                    <img class="loginImage"><i class="fa fa-info-circle" aria-hidden="true"></i> Login
-                </a>
-            </li>
-        </ul>
+<body class="bodyIndex">
+<div class="flexContainer">
+    <div class="wrapper">
+        <div class="sidebar-container">
+            <ul class="sidebar-navigation">
+                <li id="premier">
+                    <a href="#">
+                        <i class="fa fa-home" aria-hidden="true"></i> Statut
+                    </a>
+                </li>
+                <li id="deuxieme">
+                    <a href="#">
+                        <i class="fa fa-tachometer" aria-hidden="true"></i> Dernier donateur :
+                    </a>
+                </li>
+                <li id="quatrieme">
+                    <a href="#">
+                        <i class="fa fa-cog" aria-hidden="true"></i> Dernier organisateur :
+                    </a>
+                </li>
+                <li id="sixième">
+                    <a href="#">
+                        <i class="fa fa-info-circle" aria-hidden="true"></i> Top 5
+                    </a>
+                </li>
+                <li id="LoginPage">
+                    <div class="loginProfil">
+                        <a href="#">
+                            <img class="loginImage"><i class="fa fa-info-circle" aria-hidden="true"></i> Login
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
     <div class="feed">
         <form action="" class="article">
+            <p>Qu'est-ce que le Lorem Ipsum?
+                Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
 
+            </p>
+        </form>
+        <form action="" class="article">
+            <p>Qu'est-ce que le Lorem Ipsum?
+                Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+
+            </p>
+        </form>
+        <form action="" class="article">
+            <p>Qu'est-ce que le Lorem Ipsum?
+                Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+
+            </p>
         </form>
     </div>
+    <div class="user"></div>
 </div>
 </body>
 
