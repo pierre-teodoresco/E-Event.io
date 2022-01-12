0ad6302d353869a7ee6cefdf1e596419e84e0314
pierre <pierre.teodoresco@etu.univ-amu.fr>
2022-01-12 16:59:57 +0100

    link login.php

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link id="pagestyle" rel="stylesheet" type="text/css" href="css/style.css" />
-    <script type="text/javascript" src="script.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Event.io!</title>
 </head>
@@ -53,11 +52,11 @@
                 </li>
                 <li id="LoginPage">
                     <div class="loginProfil">
-                        <a href="#" class="login">
+                        <a href="login.php" class="login">
                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="icon" viewBox="0 0 15 15">
                                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                             </svg>
-                            <img class="loginImage"><i class="fa fa-info-circle" aria-hidden="true"></i> Login / Profil
+                            <img class="loginImage" alt="#" src="#"><i class="fa fa-info-circle" aria-hidden="true"></i> Login / Profil
                         </a>
                     </div>
                 </li>
