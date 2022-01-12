d11225e0e9e149b132c31c904363b9c0bf5e7cd3
pierre <pierre.teodoresco@etu.univ-amu.fr>
2022-01-12 17:21:56 +0100

    correction svg

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="icon" viewBox="0 0 15 15">
                                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                             </svg>
-                            <img class="loginImage" alt="#" src="#"><i class="fa fa-info-circle" aria-hidden="true"></i> Login / Profil
+                            <i class="fa fa-info-circle" aria-hidden="true"></i> Login / Profil
                         </a>
                     </div>
                 </li>
