37700c6a0d1ef3c521b665876e215fee4c619d2e
Théo Deshors <deshors.theo@gmail.com>
2022-01-20 21:41:51 +0100

    suppresion ancien fichier

--- 404.php
+++
@@ -1,8 +0,0 @@
-<!doctype html>
-<html><head>
-    <title>Erreur 404</title>
-</head><body>
-<h1>Page non trouvée</h1>
-<p>Nous n'avons pas pu trouver la page que vous cherchez...</p>
-<hr>
-</body></html>
--- app/Exceptions/ExceptionController.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-class ExceptionController extends Exception {}
